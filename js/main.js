d5526e46f8c17147c523ca2bd0d0c424ca96ca07
Vladimir <104868739+Travo1ta@users.noreply.github.com>
2025-03-13 13:00:57 +0300

    the functions needed to create an array of 25 generated objects

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
+
 const getRandomNum = (start, end) => {
   if (start < 0 || end < 0) {
     throw new TypeError('Допускаются только положительные числа!');
@@ -25,15 +26,88 @@ const getRandomNum = (start, end) => {
   return Math.floor(Math.random() * (end - start + 1)) + start;
 };
 
+//функция для получения рандомного элемента массива
+
+const getRandomElement = (array) => array[getRandomNum(0, array.length - 1)];
 
 //Функция для проверки максимальной длины строки.
 
-const isLengthLimit = (string, maxLength) =>  string.length <= maxLength;
+//const isLengthLimit = (string, maxLength) =>  string.length <= maxLength;
 
-isLengthLimit();
+//isLengthLimit();
 
-// функция получения случайного элемента из массива
+// Массивы с возможными данными
+const descriptions = [
+  'Прекрасный закат над городом.',
+  'Лес в утреннем тумане.',
+  'Собака играет на пляже.',
+  'Восхитительное блюдо, которое я приготовил.',
+  'Момент с друзьями на горнолыжном курорте.',
+  'Солнечный день у озера.'
+];
 
-const getRandomElement = (elements) => elements[getRandomNum(0, elements.length - 1)];
+const messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
 
-getRandomElement();
+const names = [
+  'Александр',
+  'Мария',
+  'Дмитрий',
+  'Анна',
+  'Сергей',
+  'Елена',
+  'Кирилл'
+];
+
+// Функция для генерации комментария
+const createComment = (id) => {
+  const avatarId = getRandomNum(1, 6);
+  return {
+    id: id,
+    avatar: `img/avatar-${avatarId}.svg`,
+    message: getRandomElement(messages),
+    name: getRandomElement(names)
+  };
+};
+
+// Функция для генерации массива комментариев
+const generateComments = (count) => {
+  const comments = [];
+  for (let i = 0; i < count; i++) {
+    comments.push(createComment(i + 1)); // начиная с 1
+  }
+  return comments;
+};
+
+// Функция для создания объекта фотографии
+const createPicture = (id) => {
+  const likes = getRandomNum(15, 200);
+  const commentsCount = getRandomNum(1, 5); // количество комментариев от 1 до 5
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: getRandomElement(descriptions),
+    likes: likes,
+    comments: generateComments(commentsCount)
+  };
+};
+
+// Основная функция для генерации массива фотографий
+const getPictures = () => {
+  const arr = [];
+  for (let i = 1; i <= 25; i++) {
+    const picture = createPicture(i);
+    arr.push(picture);
+  }
+  return arr;
+};
+
+// Генерация массива из 25 фотографий
+const photosArray = getPictures();
+console.log(photosArray);
